f64fe4dd9112c846c06845a9a43715aac92fa1ce
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-11-17 16:50:07 -0500

    added age label, textbox, combobox and button events

--- src/ExampleWindow.java
+++ src/ExampleWindow.java
@@ -1,13 +1,18 @@
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
 
 /**
  * Start https://youtu.be/IvZMfSR_uP4?list=PL5svY1bZDBZrHNW4_Val5LXh0_MSfxeaO
@@ -24,7 +29,11 @@ public class ExampleWindow extends JFrame implements ActionListener {
 	private JLabel _nameLabel;
 	private JTextField _nameTextField;
 	private JButton _goodbyeButton;
+	private JLabel _ageLabel;
+	private JTextField _ageTextField;
+	private JComboBox _numComboBox;
 
+	private Border _redLine, _blackLine;
 
 	// PUBLIC PROPERTIES ++++++++++++++++++++++++++++++++++++++
 	public JLabel getHelloLabel() {
@@ -40,12 +49,15 @@ public class ExampleWindow extends JFrame implements ActionListener {
 	// CONSTRUCTOR METHOD ++++++++++++++++++++++++++++++++++++++
 	public ExampleWindow() {
 		this._initialize();
+		this._setupBorders();
 		this._addUIComponents();
-		
-		// Register event handlers, where "this" references the entire class and actionPerformed method
+
+		// Register event handlers, where "this" references the entire class and
+		// actionPerformed method
 		this._nameTextField.addActionListener(this);
 		this._goodbyeButton.addActionListener(this);
-		
+		this._ageTextField.addActionListener(this);
+
 	}
 
 	// PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++
@@ -55,7 +67,11 @@ public class ExampleWindow extends JFrame implements ActionListener {
 		this._contentPane = new JPanel();
 		this._contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		this.setContentPane(this._contentPane);
+	}
 
+	private void _setupBorders() {
+		this._blackLine = BorderFactory.createLineBorder(Color.black);
+		this._redLine = BorderFactory.createLineBorder(Color.red);
 	}
 
 	private void _addHelloLabel() {
@@ -79,29 +95,62 @@ public class ExampleWindow extends JFrame implements ActionListener {
 
 		// Name Text Field
 		this._nameTextField = new JTextField();
-		this._nameTextField.setBounds(99, 35, 131, 29);
+		this._nameTextField.setBounds(99, 34, 131, 29);
+		this._nameTextField.setBorder(this._blackLine);
 		this._contentPane.add(_nameTextField);
-		
+
 		// Goodbye Button
 		this._goodbyeButton = new JButton("Click Me!");
-		this._goodbyeButton.setBounds(109, 75, 121, 23);
-		_contentPane.add(this._goodbyeButton);
+		this._goodbyeButton.setBounds(110, 202, 120, 29);
+		this._contentPane.add(this._goodbyeButton);
+
+		this._ageLabel = new JLabel("Enter Age:");
+		this._ageLabel.setBounds(5, 75, 96, 26);
+		this._contentPane.add(this._ageLabel);
+
+		this._ageTextField = new JTextField();
+		this._ageTextField.setBounds(99, 72, 131, 29);
+		this._ageTextField.setBorder(this._blackLine);
+		this._contentPane.add(this._ageTextField);
+		
+		this._numComboBox = new JComboBox();
+		this._numComboBox.setMaximumRowCount(4);
+		this._numComboBox.setEditable(true);
+		this._numComboBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5"}));
+		this._numComboBox.setSelectedIndex(0);
+		this._numComboBox.setBounds(192, 123, 38, 20);
+		this._contentPane.add(this._numComboBox);
 	}
-	
-	
-	
-	// THIRD METHOD OF HANDLING EVENTS, less control but easier access to variables?
+
+	// THIRD METHOD OF HANDLING EVENTS, less control but easier access to
+	// variables?
 	// implements ActionListener interface and add methods
-	
+	// recall, all user input is accepted as ... String! add exception handling
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		
-		if(event.getSource() == this._nameTextField){
-		this._helloLabel.setText("nameTextField - Enter Pressed");
+
+		if (event.getSource() == this._nameTextField) {
+			this._helloLabel.setText(this._nameTextField.getText());
 		}
-		
-		if(event.getSource() == this._goodbyeButton){
+
+		if (event.getSource() == this._goodbyeButton) {
 			this._helloLabel.setText("goodbyeButton - clicked");
+		}
+
+		if (event.getSource() == this._ageTextField) {
+
+			try {
+				int ageDifference = 46 - Integer.parseInt(this._ageTextField.getText());
+				this._helloLabel.setText(Integer.toString(ageDifference));
+				this._ageTextField.setBorder(this._blackLine);
+
+			} catch (Exception e) {
+				this._helloLabel.setText("Hey, tha's just wrong");
+				this._ageTextField.selectAll();
+
+				this._ageTextField.setBorder(this._redLine);
+			}
+
 		}
 	}
 }
