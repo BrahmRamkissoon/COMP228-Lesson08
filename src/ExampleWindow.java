0772878c1845812b86a9799dd5abab1dac4a3968
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-11-17 10:24:23 -0500

    added handler for name text field

--- src/ExampleWindow.java
+++ src/ExampleWindow.java
@@ -1,77 +1,80 @@
-import java.awt.BorderLayout;
-
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import java.awt.FlowLayout;
-import javax.swing.BoxLayout;
-import java.awt.GridLayout;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 /**
- *  https://youtu.be/IvZMfSR_uP4
- * @author Tom Tsiliopoulos
- *
+ * Start https://youtu.be/IvZMfSR_uP4?list=PL5svY1bZDBZrHNW4_Val5LXh0_MSfxeaO
+ * End https://youtu.be/jRE5ouam9kI?list=PL5svY1bZDBZrHNW4_Val5LXh0_MSfxeaO
+ * @author Tom Tsiliopoulos 
+ *         
  */
 // EXAMPLEWINDOW CLASS ++++++++++++++++++++++++++++++++++++++
 public class ExampleWindow extends JFrame {
-	//PRIVATE INSTANCE VARIABLES ++++++++++++++++++++++++++++++++++++++
-	private JPanel _contentPane;	// JPanel Container
-	private JLabel _helloLabel;		// access to _helloLabel
+	// PRIVATE INSTANCE VARIABLES ++++++++++++++++++++++++++++++++++++++
+	private JPanel _contentPane; // JPanel Container
+	private JLabel _helloLabel; // access to _helloLabel
 	private JLabel _nameLabel;
 	private JTextField _nameTextField;
 	
-	
-		
-	// PUBLIC PROPERTIES  ++++++++++++++++++++++++++++++++++++++
+	private NameTextFieldHandler _nameTextFieldHandler;
+
+	// PUBLIC PROPERTIES ++++++++++++++++++++++++++++++++++++++
 	public JLabel getHelloLabel() {
-		return this._helloLabel;	
+		return this._helloLabel;
 	}
 
 	public void setHelloLabel(JLabel helloLabel) {
+		this._contentPane.remove(this._helloLabel);
 		this._helloLabel = helloLabel;
+		this._addHelloLabel();
 	}
 
-
-	// 	CONSTRUCTOR METHOD ++++++++++++++++++++++++++++++++++++++
+	// CONSTRUCTOR METHOD ++++++++++++++++++++++++++++++++++++++
 	public ExampleWindow() {
 		this._initialize();
 		this._addUIComponents();
+		
+		this._nameTextFieldHandler = new NameTextFieldHandler();
+		this._nameTextField.addActionListener(this._nameTextFieldHandler);
 	}
-	
-	
+
 	// PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++
 	private void _initialize() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 300, 300);
 		this._contentPane = new JPanel();
 		this._contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		this.setContentPane(this._contentPane);	
-		
+		this.setContentPane(this._contentPane);
+
+	}
+	
+	
+	private void _addHelloLabel() {
+		this._helloLabel.setBounds(5, 6, 96, 23);
+		this._contentPane.add(this._helloLabel);
 	}
 
 	private void _addUIComponents() {
 		// Use Absolute Layout
 		this._contentPane.setLayout(null);
-		
+
 		// Hello Label
 		this._helloLabel = new JLabel("New label");
-		this._helloLabel.setBounds(5, 6, 96, 23);
-		this._contentPane.add(this._helloLabel);
-		
+		this._addHelloLabel();
+
 		// Name Label
-		this._nameLabel = new JLabel("Enter Name:");  
+		this._nameLabel = new JLabel("Enter Name:");
 		this._nameLabel.setBounds(5, 41, 96, 23);
 		this._nameLabel.setVerticalAlignment(SwingConstants.TOP);
 		this._contentPane.add(_nameLabel);
-		
+
 		// Name Text Field
 		this._nameTextField = new JTextField();
 		this._nameTextField.setBounds(99, 35, 131, 29);
 		this._contentPane.add(_nameTextField);
-		
 
 	}
 
---
+++ src/NameTextFieldHandler.java
@@ -0,0 +1,13 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class NameTextFieldHandler implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		System.out.println("event!");
+		
+		// let's try passing a reference to ExampleWindow class
+	}
+
+}
